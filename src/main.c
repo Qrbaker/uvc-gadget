74fde5f0a13d7e85f7c514c2cade27a6b5cba33f
Quentin Baker <quentin@quentb.com>
2025-05-17 16:08:48 -0700

    fix function args in bcm calls

--- src/main.c
+++ src/main.c
@@ -59,7 +59,7 @@ static void streaming_status_enable()
     // Set pin as output
 	bcm2835_gpio_fsel(RPI_GPIO_P1_21, BCM2835_GPIO_FSEL_OUTP);
 	// Set pin HIGH
-	bcm2835_gpio_write(pin, HIGH);
+	bcm2835_gpio_write(RPI_GPIO_P1_21, HIGH);
 }
 
 void streaming_status_disable()
@@ -67,7 +67,7 @@ void streaming_status_disable()
     // Set pin as output
 	bcm2835_gpio_fsel(RPI_GPIO_P1_21, BCM2835_GPIO_FSEL_OUTP);
 	// Set pin LOW
-	bcm2835_gpio_write(pin, LOW);
+	bcm2835_gpio_write(RPI_GPIO_P1_21, LOW);
 }
 
 /* Necessary for and only used by signal handler. */
