56da497bec5bb02b3716ca97ccf6c9cae9f27800
Quentin Baker <quentin@quentb.com>
2025-05-18 13:10:39 -0700

    write LED GPIO to LOW when ending stream

--- lib/stream.c
+++ lib/stream.c
@@ -276,8 +276,8 @@ static int uvc_stream_stop(struct uvc_stream *stream)
 	video_source_free_buffers(stream->src);
 
 	bcm2835_gpio_fsel(RPI_V2_GPIO_P1_40, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_write(RPI_V2_GPIO_P1_40, HIGH);
-	
+	bcm2835_gpio_write(RPI_V2_GPIO_P1_40, LOW);
+
 	return 0;
 }
 
